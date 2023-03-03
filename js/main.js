781e1680966fe2095b230348b844b141f95e3537
MichelleDuque <michelled_1994@hotmail.com>
2023-03-03 11:05:55 -0500

    javascript fixed

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ console.log('music player script file');
 
 
 // get a reference to all of the audio tags on the page
-let allAudio = document.querySelectorAll("Audio")
+let allDivs = document.querySelectorAll(".key")
 
 
 window.addEventListener("keyup", findMatchingAudio);
@@ -37,10 +37,10 @@ function findMatchingAudio(event){
 ;}
 
 function resetDivs(){
-    let currentDiv = document.querySelector(`div[data-key="${this.dataset.key}"]`)
+    // let currentDiv = document.querySelector(`div[data-key="${this.dataset.key}"]`)
 
-    currentDiv.classList.remove("playing")
+    this.classList.remove("playing")
 }
 
-allAudio.forEach(audio => audio.addEventListener("ended", resetDivs))
+allDivs.forEach(div => div.addEventListener("transitionend", resetDivs))
 
